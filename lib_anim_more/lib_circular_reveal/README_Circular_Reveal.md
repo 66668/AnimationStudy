d56eef0e0c4237fc2d23319afdd6724ac9175960
JackSong <sjy0118atsn@163.com>
2019-01-03 14:47:07 +0800

    circular reveal demo

--- lib_anim_more/lib_circular_reveal/README_Circular_Reveal.md
+++ lib_anim_more/lib_circular_reveal/README_Circular_Reveal.md
@@ -5,3 +5,45 @@ ViewAnimationUtils.createCircularReveal(View view,int centerX,  int centerY, flo
 让您能够为裁剪区域添加动画以揭露或隐藏视图。
 
 所以揭露动画很简单，使用如上方法就能实现，只需要理解方法参数含义即可。
+
+## 参数详解：
+
+ 如下方法的解释：揭露动画对象只能使用一次，不能被重新使用，也就是说每次使用揭露动画都要调用 ViewAnimationUtils.createCircularReveal() 返回一个揭露动画对象使用，
+ 同时一旦开始了动画就不能暂停或重新开始。揭露动画是一种异步动画，可以自动运行在 UI 线程上。 当揭露动画结束后，如果设置了 Animator.AnimatorListener 监听器，
+ 那么监听器的 onAnimationEnd(Animator) 方法会被调用， 但可能会被延迟调用，这取决于线程的响应能力。
+    
+    package android.view;
+    
+    import android.animation.Animator;
+    import android.animation.Animator.AnimatorListener;
+    import android.animation.RevealAnimator;
+    
+    /**
+     * 定义处理视图动画的常用工具。
+     *
+     */
+    public final class ViewAnimationUtils {
+        private ViewAnimationUtils() {}
+        /**
+         *
+         * @param view The View will be clipped to the animating circle.
+         * @param centerX The x coordinate of the center of the animating circle, relative to
+         *                <code>view</code>.
+         * @param centerY The y coordinate of the center of the animating circle, relative to
+         *                <code>view</code>.
+         * @param startRadius The starting radius of the animating circle.
+         * @param endRadius The ending radius of the animating circle.
+         */
+        public static Animator createCircularReveal(View view,
+                int centerX,  int centerY, float startRadius, float endRadius) {
+            return new RevealAnimator(view, centerX, centerY, startRadius, endRadius);
+        }
+    }
+
+1. 参数1 View view：           执行揭露动画的 View 视图
+2. 参数2 int centerX：         相对于视图 View 的坐标系，动画圆的中心的x坐标
+3. 参数3 int centerY：         相对于视图 View 的坐标系，动画圆的中心的y坐标
+4. 参数3 float startRadius：   动画圆的起始半径
+4. 参数3 float endRadius：     动画圆的结束半径
+
+![circular reveal 动画](https://github.com/66668/AnimationStudy/blob/master/interpolator_pic/createCircularReveal.jpg)
---
+++ lib_anim_more/lib_circular_reveal/src/main/java/lib/circular/reveal/CRMainAct.java
@@ -0,0 +1,91 @@
+package lib.circular.reveal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewAnimationUtils;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import lib.base.sjy.adpater.MainAdapter;
+
+
+public class CRMainAct extends AppCompatActivity {
+
+    //-----------------------------控件--------------------------------
+    //RecyclerView-v7
+    @BindView(R2.id.recyclerView)
+    RecyclerView recyclerView;
+
+    //-------------------------------------------------------------
+    private List<String> data = new ArrayList<>();
+    private MainAdapter adapter;
+    ViewAnimationUtils viewAnimationUtils;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_vrmain);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        data = new ArrayList<>();
+        //
+        data.add("xml方式1：正常控件");
+
+        //
+        adapter = new MainAdapter(this);
+        adapter.setDatas(data);
+        //
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+        //
+        recyclerView.setAdapter(adapter);
+        //
+        adapter.setListener(new MainAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(String str, int pos) {
+                switch (pos) {
+                    case 0:
+                        startToVAxmlAct();
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        Toast.makeText(CRMainAct.this, str + pos, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+
+    //
+    private void startToVAxmlAct() {
+//        Intent intent = new Intent(CRMainAct.this, TAxmlAct.class);
+//        startActivity(intent);
+    }
+
+
+}
